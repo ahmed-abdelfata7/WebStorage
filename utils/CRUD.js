2bb586e8d2bf0fadae4fe274e15c287417b5bf76
EngAhmed <ahmed.m.web.dev@gmail.com>
2019-01-26 23:23:02 +0400

    Show all tasks in table

--- utils/CRUD.js
+++ utils/CRUD.js
@@ -2,17 +2,28 @@
 var tasks = [];
 $(document).ready(() => {
     $("#task_error").hide();
+    $("#task_success").hide();
+    $("#all_tasks").hide();
 });
 //save task
 function save_task() {
     let task = $("#task").val();
     //check if task not empty
     if (task) {
+        //filter the input
+        task = task.replace(/(<([^>,]+)>)/ig, "");
         //saving task into array first then add it to our local storage
-        task = task.replace(/(<([^>]+)>)/ig, "");
-        alert(task);
-        tasks.push(task + "<br>");
+        tasks.push(task);
         localStorage.tasks = tasks;
+        //showing success msg after saving
+        $("#task").val("");
+        $("#task_success").html("Task saved successfully");
+        $("#task_success").show();
+        setTimeout(() => {
+            $("#task_success").hide();
+        }, 2000);
+        show();
+
     } else {
         $("#task_error").html("Please Enter Task Name");
         $("#task_error").show();
@@ -20,5 +31,31 @@ function save_task() {
             $("#task_error").hide();
         }, 2000);
     }
-    console.log(localStorage.tasks);
+}
+//get all tasks
+function show() {
+    $("#new_task").html("");
+    $("#all_tasks").show();
+    let saved_tasks = localStorage.tasks
+    if (saved_tasks !== undefined) {
+        let transforming_tasks = saved_tasks.split(",");
+        for (let index = 0; index <= transforming_tasks.length; index++) {
+            if (transforming_tasks[index] !== undefined && transforming_tasks[index] != "") {
+                console.log(transforming_tasks[index]);
+                $("#new_task").append(`
+                    <tr>
+                        <td>
+                        ${transforming_tasks[index]}
+                        </td>
+                        <td>
+                        <i class="fa fa-trash fa-lg" style="color:red;" onclick="task_details('${transforming_tasks[index]}');"></i>
+                        </td>
+                    </tr>
+            `);
+            }
+        }
+    }
+}
+function task_details(data) {
+
 }
